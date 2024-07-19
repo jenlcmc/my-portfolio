ba84cd5b68c53565a8568cd8dbafee1891be4600
Uyen Tran <62521466+jenlcmc@users.noreply.github.com>
2024-07-18 21:58:29 -0700

    update gitignore and minor stuff

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,7 @@ function App() {
 					count is {count}
 				</button>
 				<p>
-					Edit <code>src/App.tsx</code> and save to test HMR testingggg
+					Edit <code>src/App.tsx</code> and save to test HMR nice
 				</p>
 			</div>
 			<p className="read-the-docs">
