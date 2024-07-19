80d47c24be973c5ee586a98c4410b1a649c0f86c
Uyen Tran <62521466+jenlcmc@users.noreply.github.com>
2024-07-18 21:17:05 -0700

    test2

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,7 @@ function App() {
 					count is {count}
 				</button>
 				<p>
-					Edit <code>src/App.tsx</code> and save to test HMR testinggggg
+					Edit <code>src/App.tsx</code> and save to test HMR testingggg
 				</p>
 			</div>
 			<p className="read-the-docs">
