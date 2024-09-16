9f3da4b81d4b20e5ebe52d8ee8d471b4cfe9bd38
Uyen Tran <62521466+jenlcmc@users.noreply.github.com>
2024-09-15 20:24:20 -0700

    add spinner

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-//import React from "react";
+import { useEffect, useState } from "react";
 import Navigation from "./components/NavBar/NavBar";
 import Home from "./pages/Home";
 import About from "./pages/About";
@@ -6,31 +6,53 @@ import Projects from "./pages/Projects";
 import Education from "./pages/Education";
 import Work from "./pages/Work";
 import Footer from "./components/Footer";
+import Spinner from "./hooks/Spinner";
 
 import "./index.css";
 
-const App = () => (
-	<div className="min-h-screen flex flex-col ">
-		<Navigation />
-		<main className="flex-grow container mx-auto p-4">
-			<div id="home">
-				<Home />
-			</div>
-			<div id="about">
-				<About />
-			</div>
-			<div id="education">
-				<Education />
-			</div>
-			<div id="work">
-				<Work />
-			</div>
-			<div id="projects">
-				<Projects />
-			</div>
-		</main>
-		<Footer />
-	</div>
-);
+const App = () => {
+	const [loading, setLoading] = useState(true);
+
+	useEffect(() => {
+		// Simulate loading time (for example, 2 seconds)
+		const timer = setTimeout(() => {
+			setLoading(false);
+		}, 2000);
+
+		return () => clearTimeout(timer); // Cleanup timeout when the component unmounts
+	}, []);
+
+	return (
+		<div className="min-h-screen flex flex-col">
+			{loading ? (
+				<Spinner />
+			) : (
+				<div>
+					{" "}
+					{/* Replaced <> with <div> */}
+					<Navigation />
+					<main className="flex-grow container mx-auto p-4">
+						<div id="home">
+							<Home />
+						</div>
+						<div id="about">
+							<About />
+						</div>
+						<div id="education">
+							<Education />
+						</div>
+						<div id="work">
+							<Work />
+						</div>
+						<div id="projects">
+							<Projects />
+						</div>
+					</main>
+					<Footer />
+				</div>
+			)}
+		</div>
+	);
+};
 
 export default App;
---
+++ src/hooks/Spinner.tsx
@@ -0,0 +1,12 @@
+//loading spinner
+import "../index.css";
+
+const Spinner = () => {
+	return (
+		<div className="flex items-center justify-center h-screen">
+			<div className="loader ease-linear rounded-full border-4 border-t-4 border-gray-200 h-12 w-12"></div>
+		</div>
+	);
+};
+
+export default Spinner;
