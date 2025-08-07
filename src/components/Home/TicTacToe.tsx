da5395e0ab6656f9a4fa61cbc6c360ccec69b73c
Uyen Tran <62521466+jenlcmc@users.noreply.github.com>
2025-08-06 17:05:59 -0700

    deploy webste with chages

--- src/components/Home/TicTacToe.tsx
+++ src/components/Home/TicTacToe.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 
 // Define types for the square values and the square props
 type SquareValue = 'X' | 'O' | null;
